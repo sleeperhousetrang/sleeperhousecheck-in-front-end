a4b834202eefeaddca3e8bdb9bda7e9957338b5d
Anders Chen <anders@wistia.com>
2025-02-10 13:43:21 +0700

    Implement form submission success state

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: ["./index.html"],
   safelist: [
     "border-red-500",
-    "bg-gray-500"
+    "bg-gray-500",
+    "text-color-green-500"
   ],
   theme: {
     extend: {},
